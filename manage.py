d95e5f5ba8bad863953ed4ab413df9876c38d3a7
oerijogoo <joerironald@gmail.com>
2025-07-14 23:10:26 +0300

    school renders

--- manage.py
+++ manage.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 from pathlib import Path
 
 
 def main():
-    """Run administrative tasks."""
-    # Add the project base directory to the Python path
-    BASE_DIR = Path(__file__).resolve().parent
-    sys.path.append(str(BASE_DIR))
+    # Add the parent directory to Python path (critical fix)
+    sys.path.append(str(Path(__file__).parent.parent))
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment variable? "
-            "Did you forget to activate a virtual environment?"
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
